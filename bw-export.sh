ad21d1b68a6a949cc6e0569e10f505904d338bd3
Guru <mail@gururaja.in>
2024-09-08 02:35:59 +0530

    typo

--- bw-export.sh
+++ bw-export.sh
@@ -13,9 +13,9 @@ bw export --format encrypted_json --password "${BW_PASSWORD}" --output "${file_n
 
 # Dropbox Uploader configuration
 echo "CONFIGFILE_VERSION=2.0" > ~/.dropbox_uploader
-echo "OAUTH_APP_KEY=${dropbox_app_key}" >> ~/.dropbox_uploader
-echo "OAUTH_APP_SECRET=${dropbox_app_secret}" >> ~/.dropbox_uploader
-echo "OAUTH_REFRESH_TOKEN=${dropbox_refresh_token}" >> ~/.dropbox_uploader
+echo "OAUTH_APP_KEY=${DROPBOX_APP_KEY}" >> ~/.dropbox_uploader
+echo "OAUTH_APP_SECRET=${DROPBOX_APP_SECRET}" >> ~/.dropbox_uploader
+echo "OAUTH_REFRESH_TOKEN=${DROPBOX_REFRESH_TOKEN}" >> ~/.dropbox_uploader
 
 # Download Dropbox Uploader script
 curl -s "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
